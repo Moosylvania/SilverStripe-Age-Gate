beb2f5a41471f4b9ef3c194c3a46b7a87278ee33
Joe Madden <obj63mc@gmail.com>
2018-05-04 14:49:07 -0500

    setup for more configuration and options with birthdate field

--- README.md
+++ README.md
@@ -5,21 +5,40 @@ Simple Age Gate module for SilverStripe, ensures that users entering site are 21
 
 ## Requirements
 
- * SilverStripe 4.0.0+, for 3.x checkout 3.x branch
+* SilverStripe 4.0.0+, for 3.x checkout 3.x branch
+
 
 ## Installation & Documentation
 
- 1. Clone Repository to your SilverStripe Project
- 2. Add the following your sites app config.yml/mysite.yml etc...
+1. Clone Repository to your SilverStripe Project or -
+
+        composer require moosylvania/silverstripe-age-gate
+
+2. Add the following your sites app config.yml/mysite.yml etc...
 
         SilverStripe\CMS\Controllers\ContentController:
           extensions:
             - Moosylvania\AgeGate\Extensions\AgeGateExtension
 
- 3. This will add a route /age-gate to your project and all users will be presented with a yes/no button to validate of age.
- 4. You can set up where you want a user redirected to if they select no by adding the following config value to your site -
+3. This will add a route /age-gate to your project and all users will be presented with a yes/no button to validate of age.  If you would like the user to type in their date of birth you can set the config option of 'askForAge'
+
+        Moosylvania\AgeGate\Controllers\AgeGateController:
+          askForAge: true
+
+    To set the format of the dates you can change the config values 'dateFormat' (for the form intro text) and 'dateFieldFormat' for the actual SilverStripe Date Field.
+
+        Moosylvania\AgeGate\Controllers\AgeGateController:
+          dateFormat: 'm/d/Y'
+          dateFieldFormat: 'MM/dd/yyyy'
+
+4. You can set up where you want a user redirected to if they select 'No' (not of age) by adding the following config value to your site -
 
         Moosylvania\AgeGate\Controllers\AgeGateController:
           notOfAgeRedirect: 'https://moosylvania.com'
 
-5. Run a dev/build?flush=1 and your site will now be agegated.
+5. You can also setup how old someone needs to be to enter your site (default is 21 by editing the setting)
+
+Moosylvania\AgeGate\Controllers\AgeGateController:
+  yearsOld: 21
+
+6. Run a dev/build?flush=1 and your site will now be agegated.
--- src/Controllers/AgeGateController.php
+++ src/Controllers/AgeGateController.php
@@ -6,16 +6,38 @@ use SilverStripe\Control\Cookie;
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Forms\DateField;
 use SilverStripe\Forms\Form;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\FormAction;
 use SilverStripe\Forms\LiteralField;
+use SilverStripe\Forms\RequiredFields;
 
 class AgeGateController extends PageController{
     /**
      * @config
      */
     private static $notOfAgeRedirect = 'https://google.com';
+
+    /**
+     * @config
+     */
+    private static $yearsOld = 21;
+
+    /**
+     * @config
+     */
+    private static $askForAge = false;
+
+    /**
+     * @config
+     */
+    private static $dateFormat = 'm/d/Y';
+
+    /**
+     * @config
+     */
+    private static $dateFieldFormat = 'MM/dd/yyyy';
 
     private static $allowed_actions = [
         'AgeGateForm'
@@ -47,26 +69,48 @@ class AgeGateController extends PageController{
 
     public function AgeGateForm()
     {
-        $t = strtotime("-21 year", time());
+        $timestring = "-".$this->config()->get('yearsOld')." years";
+        $t = strtotime($timestring, time());
 
         $fields = new FieldList(
-            LiteralField::create('OfAge', '<h1>Born Before '.date('m/d/Y', $t).'</h1>')
+            LiteralField::create('OfAge', '<h1>Born Before '.date($this->config()->get('dateFormat'), $t).'</h1>')
         );
 
-        $actions = new FieldList(
-            FormAction::create('doOfAge', 'Yes'),
-            FormAction::create('doNotOfAge', 'No')
-        );
+        if($this->config()->get('askForAge')){
+            $fields->push($dateField = DateField::create('birthdate', 'Your Birthdate')
+                ->setMaxDate($timestring)
+                ->setHTML5(false)
+                ->setDateFormat($this->config()->get('dateFieldFormat'))
+            );
 
-        $form = new Form($this, 'AgeGateForm', $fields, $actions);
+            $dateField->setDescription(_t(
+                'FormField.Example',
+                'e.g. {format}',
+                [ 'format' =>  $dateField->getDateFormat() ]
+            ));
 
-        $form->setValidationExemptActions(['doOfAge', 'doONotfAge']);
+            $actions = new FieldList(
+                FormAction::create('doAgeValidation', 'Submit')
+            );
+
+            $required = new RequiredFields('birthdate');
+
+            $form = new Form($this, 'AgeGateForm', $fields, $actions, $required);
+        } else {
+            $actions = new FieldList(
+                FormAction::create('doOfAge', 'Yes'),
+                FormAction::create('doNotOfAge', 'No')
+            );
+
+            $form = new Form($this, 'AgeGateForm', $fields, $actions);
+
+            $form->setValidationExemptActions(['doOfAge', 'doONotfAge']);
+        }
 
         return $form;
     }
 
-    public function doOfAge($data, Form $form)
-    {
+    protected function setOfAgeCookie(){
         Cookie::set('moosylvaniaAgeGateOfAge', true, 30);
         $request = Injector::inst()->get(HTTPRequest::class);
         $session = $request->getSession();
@@ -75,6 +119,16 @@ class AgeGateController extends PageController{
         } else {
             return $this->redirect('/');
         }
+    }
+
+    public function doOfAge($data, Form $form)
+    {
+        $this->setOfAgeCookie();
+    }
+
+    public function doAgeValidation($data, Form $form)
+    {
+        $this->setOfAgeCookie();
     }
 
     public function doNotOfAge($data, Form $form)
